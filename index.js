830616b70f698a121f75b24776bdfb137fbe99df
John Gaitho <jgaitho016@gmail.com>
2024-10-07 18:51:12 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const original = document.querySelector('#main')
+main.remove();
+
+const newHeader = document.createElement("h1")
+
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "John-Gaitho is the champion"
